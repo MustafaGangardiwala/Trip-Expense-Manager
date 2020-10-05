d4388db6424051bc567a734c63e5bf05dd9cf45e
umang goti <umanggoti1@gmail.com>
2020-10-05 20:57:36 +0530

    SPLASH SCREEN ADDED

---
+++ app/src/main/java/com/example/tripexpensemanager/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.tripexpensemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreen.this, HomeActivity.class);
+                startActivity (homeIntent);
+                finish();
+            }
+        },2000);
+    }
+}
